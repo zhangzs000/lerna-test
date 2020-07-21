23eef80b25f2d2a880a451e94d6bd571c7b4afd3
zhangzesheng <1461998312@qq.com>
2020-07-21 09:36:35 +0800

    feat: lint fix

--- packages/umiapp/src/pages/layouts/index.tsx
+++ packages/umiapp/src/pages/layouts/index.tsx
@@ -1,19 +1,19 @@
-import React from 'react';
-import { Link } from 'umi';
+import React from 'react'
+import { Link } from 'umi'
 
 export default (props: any) => {
-    return (
-        <div style={{ padding: 20 }}>
-            <div>
-                <Link to='/adminList/list'>listPage</Link>
-                {'   '}
-                <Link to='/adminList/admin'>adminPage</Link>
-                {'   '}
-                {/* <Link to="/user">userPage</Link>{'   '}
+ return (
+  <div style={{ padding: 20 }}>
+   <div>
+    <Link to='/adminList/list'>listPage</Link>
+    {'   '}
+    <Link to='/adminList/admin'>adminPage</Link>
+    {'   '}
+    {/* <Link to="/user">userPage</Link>{'   '}
         <Link to="/login">loginPage</Link> */}
-            </div>
+   </div>
             common layout
-            {props.children}
-        </div>
-    );
-};
+   {props.children}
+  </div>
+ )
+}
--- packages/umiapp/src/pages/list/index.tsx
+++ packages/umiapp/src/pages/list/index.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
-import styles from './index.less';
-import Counter from '@/components/counter';
+import React from 'react'
+import styles from './index.less'
+import Counter from '@/components/counter'
 export default () => {
-    return (
-        <div>
-            <h1 className={styles.title}>Page list</h1>
-            <Counter />
-        </div>
-    );
-};
+ return (
+  <div>
+   <h1 className={styles.title}>Page list</h1>
+   <Counter />
+  </div>
+ )
+}
