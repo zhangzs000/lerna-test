317191c63b9b456e82c7e1aabecaf17e6044b75e
zhangzesheng <1461998312@qq.com>
2020-07-27 12:00:52 +0800

    feat: change gulp 4

--- packages/test-lerna-copy-module/gulpfile.js
+++ packages/test-lerna-copy-module/gulpfile.js
@@ -1,12 +1,13 @@
-var gulp = require('gulp');
-var uglify = require('gulp-uglify');
-var rename = require('gulp-rename');
+var gulp = require('gulp')
+var uglify = require('gulp-uglify')
+var rename = require('gulp-rename')
 
-gulp.task('default', function () {
-    gulp.src('src/*.js')
-        .pipe(uglify().on('error', function(e){
-            console.log('error: ',e);
-         }))
-        .pipe(rename({ extname: '.min.js' }))
-        .pipe(gulp.dest('dist/'))
-});
+gulp.task('default', async function () {
+ gulp.src('src/*.js')
+  .pipe(uglify().on('error', function (e) {
+   // eslint-disable-next-line no-console
+   console.log('error: ', e)
+  }))
+  .pipe(rename({ extname: '.min.js' }))
+  .pipe(gulp.dest('dist/'))
+})
