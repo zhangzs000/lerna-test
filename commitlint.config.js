19c2d4044d2c4abc822fc122740372ce530c1fa0
zhangzesheng <1461998312@qq.com>
2020-07-27 17:42:00 +0800

    feat: add lernachangelog

--- commitlint.config.js
+++ commitlint.config.js
@@ -3,7 +3,7 @@ module.exports = {
  rules: {
   'header-max-length': [2, 'always', 120],
   'subject-case': [0],
-  'type-enum': [2, 'always', ['release', 'docs', 'chore', 'feat', 'fix', 'merge', 'perf', 'refactor', 'revert', 'style', 'test']],
+  'type-enum': [2, 'always', ['release', 'docs', 'chore', 'feat', 'fix', 'merge', 'perf', 'refactor', 'revert', 'style', 'test', 'doc', 'bug', 'enhancement']],
   'scope-empty': [0]
  }
 }
